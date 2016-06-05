4d0943695d40da32004e81fe884309f4e3739359
Viktor Smari <viktorsmari@gmail.com>
2016-06-05 18:38:46 +0200

    Use angular

---
+++ script.js
@@ -0,0 +1,39 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', function($scope){
+
+  $scope.websites = [
+    { 
+      url: "https://x.piratar.is",
+      name: "Kosningakerfi"
+    },
+    {
+      url: "http://discourse.piratar.is",
+      name: "Málefnaspjall"
+    },
+    { 
+      url: "http://gogn.piratar.is",
+      name: "Mæting þingmanna á nefndarfundi"
+    },
+    { 
+      url: "http://www.piratar.is/althingisrynirinn/",
+      name: "Alþingisrýnir (2013)"
+    },
+    { 
+      url: "https://www.ventill.is/",
+      name: "Öryggisventill"
+    },
+    {
+      url: "http://kvika.piratar.is/",
+      name: "Kvika - Wiki vefur, inniheldur að mestu vinnuskjöl"
+    },
+    { 
+      url: "http://blog.piratar.is/",
+      name: "Blogsíður Pírata"
+    }
+
+
+
+
+  ];
+})
