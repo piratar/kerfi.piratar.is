ec6d92a1c8939fa64b026a1f85c51792ad2c7f1e
Viktor Smári <viktorsmari@users.noreply.github.com>
2018-11-05 11:00:27 +0100

    Reorder by most used

--- script.js
+++ script.js
@@ -9,45 +9,19 @@ myApp.controller('mainController', function($scope){
       person: "Helgi Hrafn Gunnarsson"
     },
     {
-      url: "https://pad.piratar.is/",
-      name: "Etherpad Pírata",
-      person: "?"
-    },
-
-    {
-      url: "https://member.piratar.is",
-      name: "Félagatal Pírata",
-      person: "Helgi Hrafn Gunnarsson"
-    },
-    {
-      url: "https://grafana.piratar.is",
-      name: "Yfirlit yfir kerfisnotkun? (Grafana)",
-      person: "Viktor Smári"
-    },
-    {
-      url: "http://kvika.piratar.is/",
-      name: "Kvika - Wiki vefur, inniheldur að mestu vinnuskjöl",
-      person: "?"
-    },
-    {
-      url: "https://log.piratar.is",
-      name: "Lög Pírata",
-      person: "(Viktor Smári)"
-    },
-    {
-      url: "https://postur.piratar.is/",
-      name: "Póstkerfi Pírata",
-      person: "Björn Þór Jóhannesso"
-    },
-    {
-      url: "http://gogn.piratar.is",
-      name: "Mæting þingmanna á nefndarfundi",
+      url: "http://blog.piratar.is/",
+      name: "Blogsíður Pírata",
       person: "?"
     },
     {
       url: "https://spjall.piratar.is",
       name: "Málefnaspjall (discource)",
       person: "?"
+    },
+    {
+      url: "https://log.piratar.is",
+      name: "Lög Pírata",
+      person: "(Viktor Smári)"
     },
     {
       url: "https://github.com/piratar/stefna",
@@ -60,6 +34,36 @@ myApp.controller('mainController', function($scope){
       person: "?"
     },
     {
+      url: "https://postur.piratar.is/",
+      name: "Póstkerfi Pírata",
+      person: "Björn Þór Jóhannesso"
+    },
+    {
+      url: "https://pad.piratar.is/",
+      name: "Etherpad Pírata",
+      person: "?"
+    },
+    {
+      url: "https://member.piratar.is",
+      name: "Félagatal Pírata",
+      person: "Helgi Hrafn Gunnarsson"
+    },
+    {
+      url: "http://kvika.piratar.is/",
+      name: "Kvika - Wiki vefur, inniheldur að mestu vinnuskjöl",
+      person: "?"
+    },
+    {
+      url: "https://grafana.piratar.is",
+      name: "Yfirlit yfir kerfisnotkun? (Grafana)",
+      person: "Viktor Smári"
+    },
+    {
+      url: "http://gogn.piratar.is",
+      name: "Mæting þingmanna á nefndarfundi",
+      person: "?"
+    },
+    {
       url: "https://discordapp.com/channels/329655342977908737/329660468694351872",
       name: "Discord",
       person: "Sigmundur Þórir Jónsson"
@@ -68,11 +72,6 @@ myApp.controller('mainController', function($scope){
       url: "http://www.piratar.is/althingisrynirinn/",
       name: "Alþingisrýnir (2013)",
       person: "Bjarni Rúnar Einarsson"
-    },
-    {
-      url: "http://blog.piratar.is/",
-      name: "Blogsíður Pírata",
-      person: "?"
     }
   ];
 })
