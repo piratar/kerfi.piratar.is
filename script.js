94b68e6485d269a4a65673d1ac9ea9e8a4516075
Helgi Hrafn Gunnarsson <helgi@binary.is>
2023-08-03 14:58:30 +0000

    Updated list of active systems in use.

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ myApp.controller('mainController', function($scope){
 
   $scope.websites = [
     {
+      url: "https://piratar.is/",
+      name: "Vefur Pírata",
+      person: "Atli Stefán Yngvason <atli.stefan@piratar.is>",
+      technology: "WordPress",
+    },
+    {
       url: "https://x.piratar.is",
       name: "Kosningakerfi Pírata",
       person: "Helgi Hrafn Gunnarsson <helgi@piratar.is>",
       technology: "Wasa2il",
     },
+    /*
     {
       url: "http://blog.piratar.is/",
       name: "Blogsíður Pírata",
       person: "Björn Þór Jóhannesson <bjorn@piratar.is>",
       technology: "WordPress",
     },
+    */
     {
       url: "https://spjall.piratar.is",
       name: "Málefnaspjall Pírata",
@@ -27,49 +35,61 @@ myApp.controller('mainController', function($scope){
       person: "Helgi Hrafn Gunnarsson <helgi@piratar.is>",
       technology: "Nextcloud",
     },
+    /*
     {
       url: "https://log.piratar.is",
       name: "Lög Pírata",
       person: ""
     },
+    */
+    /*
     {
       url: "https://github.com/piratar/stefna",
       name: "Stefna Pírata",
       person: "?"
     },
+    */
+    /*
     {
       url: "https://github.com/piratar/kennsla",
       name: "Kennsluefni Pírata",
       person: "?"
     },
+    */
     {
       url: "https://postur.piratar.is/",
       name: "Póstkerfi Pírata",
       person: "Björn Þór Jóhannesson <bjorn@piratar.is>",
       technology: "Zimbra",
     },
+    /*
     {
       url: "https://pad.piratar.is/",
       name: "Etherpad Pírata",
       person: "Björn Þór Jóhannesson <bjorn@piratar.is>",
       technology: "EtherPad",
     },
+    */
     {
       url: "https://member.piratar.is",
       name: "Félagatal Pírata",
       person: "Helgi Hrafn Gunnarsson <helgi@piratar.is>",
       technology: "IcePirate",
     },
+    /*
     {
       url: "http://kvika.piratar.is/",
       name: "Kvika - Wiki vefur, inniheldur að mestu vinnuskjöl",
       person: "?"
     },
+    */
+    /*
     {
       url: "https://grafana.piratar.is",
       name: "Yfirlit yfir kerfisnotkun? (Grafana)",
       person: "Viktor Smári"
     },
+    */
     /*
     {
       url: "http://gogn.piratar.is",
@@ -77,12 +97,14 @@ myApp.controller('mainController', function($scope){
       person: "?"
     },
     */
+    /*
     {
       url: "https://discordapp.com/channels/329655342977908737/329660468694351872",
       name: "Discord",
       person: "Sigmundur Þórir Jónsson",
       technology: "Discord",
     },
+    */
     /*
     {
       url: "http://www.piratar.is/althingisrynirinn/",
